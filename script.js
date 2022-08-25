6deb8fae504ceed2ea84c43013f0fc0be714b58b
Natalia Henke <henke.cruz@gmail.com>
2022-08-25 19:15:38 -0300

    segunda versão

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function maior(){
     var numero2 = parseFloat(document.getElementById("num2").value);
     
     if (numero1 > numero2)
-        alert("O primeiro número (" + numero1 + ") é maior que o segundo (" + numero2 + ")");
+        alert("Falso! O primeiro número é maior que o segundo");
     else
         if(numero2 > numero1)
-           alert("O segundo número (" + numero2 + ") é maior que o primeiro (" + numero1 + ")");
+           alert("Verdadeiro! O segundo número é maior que o primeiro");
         else
            alert("Os dois números são iguais!");
         }
